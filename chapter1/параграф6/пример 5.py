306b00c9e38b45b25b96510bb0c593694ae70830
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2020-12-29 18:41:53 +0300

    Create пример 5.py

---
+++ chapter1/параграф6/пример 5.py
@@ -0,0 +1,8 @@
+import pyautogui as pg  # перед использованием скачать pyautogui
+import _winapi as win
+import os
+from time import sleep
+open('hello user.txt', 'w+')
+os.startfile('hello user.txt')
+sleep(1)
+pg.write('hello, world!', 0.2)
