fe621d17bf0cfe7e449f5216e452552320bec1d3
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2021-01-07 11:51:03 +0300

    Create autoCommit.py

---
+++ autoCommit.py
@@ -0,0 +1,10 @@
+import pyautogui as pg
+from time import sleep
+
+sleep(3)
+for i in range(469):
+    pg.moveTo(17, 152)
+    pg.leftClick()
+    pg.moveTo(131, 974)
+    pg.leftClick()
+    sleep(2)
