9d23e712ea2e0a3e01e4a59fa0602485e01319da
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2021-03-22 22:40:43 +0300

    Create пример1.py

---
+++ наши примеры/пример1.py
@@ -0,0 +1,14 @@
+import math
+import math as mt
+from math import *  #импортирет все из модуля math
+from math import sqrt
+
+
+# однострочный комментарий
+"""
+многострочный комментарий
+"""
+math.sqrt(144)
+mt.sqrt(144)
+sqrt(144)
+sin(45)
