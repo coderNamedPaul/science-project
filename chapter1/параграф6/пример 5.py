9ce097db1d63f78dec8ccc2a8cff3df3dc16f110
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2021-03-22 22:39:06 +0300

    Delete пример 5.py

--- chapter1/параграф6/пример 5.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as pg  # перед использованием скачать pyautogui
-import os
-from time import sleep
-open('hello user.txt', 'w+')
-os.startfile('hello user.txt')
-sleep(1)
-pg.write('hello, world!', 0.2)
