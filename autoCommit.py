c39118dde3cda7306cc4d335a628b7b23d6fe135
Rodion <rodion.dreval@gmail.com>
2021-07-10 23:18:36 +0300

    Update autoCommit.py

--- autoCommit.py
+++ autoCommit.py
@@ -2,9 +2,9 @@ import pyautogui as pg
 from time import sleep
 
 sleep(3)
-for i in range(50):
-    pg.moveTo(17, 152)
+for i in range(1000):
+    pg.moveTo(17, 175)
     pg.leftClick()
-    pg.moveTo(131, 974)
+    pg.moveTo(131, 980)
     pg.leftClick()
     sleep(4)
